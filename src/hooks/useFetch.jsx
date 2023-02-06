8273323d2aab818a168a5ed459159f333f76ba0f
HRIDAYESH PADALKAR <hridayeshpadalkar7@gmail.com>
2023-02-06 12:13:54 +0530

    Changed configs

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const APIKEY = b1rHySlchF22Lihm88H2lFrgX5jB1GMO;
+const APIKEY ='b1rHySlchF22Lihm88H2lFrgX5jB1GMO'; 
 
 const useFetch = ({ keyword }) => {
   const [gifUrl, setGifUrl] = useState("");
